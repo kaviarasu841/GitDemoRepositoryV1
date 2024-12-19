ad13c2186728538a14e749e3098648f00e3e19da
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-12-19 16:17:55 +0530

    Revert "New condition added"
    This reverts commit 314af30b39f6377865a5ba039cf762b86f8cc3fe.

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String args[]) {
 		String hungry = "yes";
 		
-		if(hungry == "yes" || hungry == "YES")
+		if(hungry == "yes")
 			System.out.println("eat something....");
 		else if (hungry == "No") {
 			System.out.println("Do something....");
